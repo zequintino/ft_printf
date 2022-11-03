75ddb6c8a5b0bbaff1a2bf59706c3d3f53c3195f
jquintin <108005850+zequintino@users.noreply.github.com>
2022-11-03 21:20:16 +0000

    putnbr_base wip

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -6,14 +6,13 @@
 /*   By: jquintin <jquintin@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 13:02:54 by jquintin          #+#    #+#             */
-/*   Updated: 2022/11/03 17:38:00 by jquintin         ###   ########.fr       */
+/*   Updated: 2022/11/03 21:19:00 by jquintin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
 int	check_valid_base(char *base);
-static int	s_len(int n);
 
 int	ft_putchar(int c)
 {
@@ -31,54 +30,42 @@ int	ft_puts(const char *s)
 	return (len);
 }
 
-int	ft_itoa(long long n, char	*base)
+int	ft_putnbr_base(long long n, char *base)
 {
-	char	*str;
-	int		i_len;
+	long long	base_len;
+	int			str_len;
+	char		tmp[50];
+	int			tmp_i;
 
+	str_len = 0;
+	tmp_i = 0;
+	base_len = ft_strlen(base);
 	if (!check_valid_base(base))
-		return(0);
-	i_len = itoa_len(n);
-	str = (char *)malloc(sizeof(char) * (i_len + 1));
-	if (!str)
-		return (NULL);
-	str[i_len] = '\0';
-	if (n < 0)
+		return (0);
+	else
 	{
-		n *= -1;
-		*str = '-';
+		if (n < 0)
+		{
+			n *= -1;
+			str_len += ft_putchar('-');
+		}
+		if (n >= base_len)
+		{
+			while (n)
+			{
+				tmp[tmp_i] = base[n % base_len];
+				n /= base_len;
+				tmp_i++;
+			}
+		}
+		else
+			return (str_len += ft_putchar(base[n]));
 	}
-	while (n > 9)
-	{
-		str[--i_len] = (n % ft_strlen(base)) + '0';
-		n /= ft_strlen(base);
-	}
-	if (n >= 0 && n <= 9)
-		str[i_len - 1] = n + '0';
-	ft_puts(str);
-	free(str);
-	return (itoa_len(n));
+	while (tmp_i >= 0)
+		str_len += ft_putchar(tmp[--tmp_i]);
+	return (str_len - 1);
 }
 
-int	itoa_len(int n)
-{
-	int	len;
-
-	len = 0;
-	if (n < 0)
-	{
-		len += 1;
-		n *= -1;
-	}
-	while (n > 9)
-	{
-		n /= 10;
-		len += 1;
-	}
-	if (n >= 0 && n <= 9)
-		return (len + 1);
-	return (len);
-}
 int	check_valid_base(char *base)
 {
 	int	i;
@@ -103,32 +90,3 @@ int	check_valid_base(char *base)
 	}
 	return (1);
 }
-
-/* int	ft_putnbr_base(long long n, char *base)
-{
-	long long	base_len;
-	int			str_len;
-
-	str_len = 0;
-	base_len = ft_strlen(base);
-	if (!check_valid_base(base))
-		return (0);
-	else
-	{
-		if (n <= INT_MIN)
-			str_len = ft_puts("-2147483648");
-		else if (n < 0)
-		{
-			ft_putchar('-');
-			str_len += ft_putnbr_base(-n, base);
-		}
-		else if (n >= base_len)
-		{
-			str_len += ft_putnbr_base(n / base_len, base);
-			str_len += ft_putnbr_base(n % base_len, base);
-		}
-		else
-			str_len += ft_putchar(base[n]);
-	}
-	return (str_len);
-} */
